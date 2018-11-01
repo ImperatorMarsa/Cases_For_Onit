772c7edafcc6d61d1d9191470d07b4c1a4ec703b
ImperatorMarsa <tima.sitnikov@mail.ru>
2018-11-01 19:27:27 +0300

    Add files via upload

---
+++ Cases_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a="";
+            for (; a.Length<100; a+="a");            
+            
+            Console.WriteLine(a.Length.ToString()+" "+a);
+        }
+    }
+}

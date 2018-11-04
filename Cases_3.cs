1f700c804773ae2f2a873a3ad1badef196bf8f6d
ImperatorMarsa <tima.sitnikov@mail.ru>
2018-11-04 20:57:34 +0300

    Add files via upload

--- Cases_3.cs
+++ Cases_3.cs
@@ -1,17 +1,16 @@
-﻿namespace xb9d8bb5e6df032aa{ //xb9d8bb5e6df032aa_
+﻿namespace Pony{ //xb9d8bb5e6df032aa_
     using System;
-    public class x1110bdd110cdcea4{ //x1110bdd110cdcea4_
-        public x1110bdd110cdcea4() : base(){}
+    public class Program{ //x1110bdd110cdcea4_
+        public Program() : base(){}
 
-        public static string _xaacba899487bce8c (string x5e99b576d2530d13, int x2710752c36f2d14b){
-            ushort usht=(ushort)x2710752c36f2d14b;;
+        public static string Fun2 (string peremen_1, int peremen_2){
+            ushort usht=(ushort)peremen_2;;
             int i=0;
-            char[] arrch=new char[x5e99b576d2530d13.Length / 4];
+            char[] arrch=new char[peremen_1.Length / 4];
             ushort usht1;
             string str;
-
-            for(i=0; i<(x5e99b576d2530d13.Length/4); i++){
-                usht1=(ushort)((((x5e99b576d2530d13[4*i]-'a')+((x5e99b576d2530d13[(4*i)+1]-'a') << '\u0004'))+((x5e99b576d2530d13[(4*i)+2]-'a') << '\u0008'))+((x5e99b576d2530d13[(4*i)+3]-'a')<<'\u000c'));
+            for(i=0; i<(peremen_1.Length/4); i++){
+                usht1=(ushort)((((peremen_1[4*i]-'a')+((peremen_1[(4*i)+1]-'a') << '\u0004'))+((peremen_1[(4*i)+2]-'a') << '\u0008'))+((peremen_1[(4*i)+3]-'a') << '\u000c'));
                 usht1-=usht;
                 arrch[i]=(char)usht1;
                 usht+=1789;
@@ -19,8 +18,9 @@
             str=new string(arrch);
             return str;
         }
+
         public static void Main(){
-            Console.WriteLine(_xaacba899487bce8c("eflohgcpkgjpegaapahapboajbfbdbmbhedcbekcgfbdkphdlepdgegekdnepcefhdlfiobggcjgadahd"+
+            Console.WriteLine(Fun2("eflohgcpkgjpegaapahapboajbfbdbmbhedcbekcgfbdkphdlepdgegekdnepcefhdlfiobggcjgadahd"+
                                    "chhmnnhncfiobmiibdjanjjcoakkmhklapkfaglkbnloldmcalmkacngpinfppnepgomknojpeplolpf"+
                                    "pcajojamoabonhbhjobmnfccnmcboddlikdnmbedniegmpeemgfamnfhiegghlghkchiljhglainlhih"+
                                    "goififjdkmjnkdkalkkmkblffilehplojgmeknmjeennjlnmicomijojiapkdhpihopaifaiimapgdbe"+
